9c3f2507fc59d74df9e21bca8b408cd1f9287bde
Joan Quillotay <joanquillotay@gmail.com>
2026-05-12 22:12:13 -0300

    feat:agrego resolucion de Punto C

---
+++ Punto_C.sh
@@ -0,0 +1,5 @@
+sudo groupadd p1c1_2025_g1
+sudo groupadd p1c1_2025_gTodos
+sudo useradd -m -G p1c1_2025_g1 -p 'Joan0609' p1c1_2025_u1
+sudo useradd -m -p 'Joan0609' p1c1_2025_u2
+sudo useradd -m -G p1c1_2025_gTodos -p '$y$j9T$Y0jhGbdG.YHtCnG5c15a8Xve$IKSPs3TUtD/L/qcgMddOne8XjwVpz2pNnlWKAsVazP.' p1c1_2025_u3
