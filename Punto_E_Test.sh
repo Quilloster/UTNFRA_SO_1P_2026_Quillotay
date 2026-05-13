cd5e9f644dc870e1d531716c5c7a801ca33ef4ed
Joan Quillotay <joanquillotay@gmail.com>
2026-05-13 20:04:40 -0300

    feat: Punto E Test completo

---
+++ Punto_E_Test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "--- Prueba u1 ---"
+sudo -u p1c1_2025_u1 ls /Punto_D/Libros
+sudo -u p1c1_2025_u1 ls /Punto_D/Musica
+sudo -u p1c1_2025_u1 ls /Punto_D/videos
+
+echo "--- Prueba u2 ---"
+sudo -u p1c1_2025_u2 ls /Punto_D/Libros
+sudo -u p1c1_2025_u2 ls /Punto_D/Musica
+sudo -u p1c1_2025_u2 ls /Punto_D/videos
+
+echo "--- Prueba u3 ---"
+sudo -u p1c1_2025_u3 ls /Punto_D/Libros
+sudo -u p1c1_2025_u3 ls /Punto_D/Musica
+sudo -u p1c1_2025_u3 ls /Punto_D/videos
